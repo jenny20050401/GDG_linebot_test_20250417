1b9a4b53c8b0854efdea8e8930495a1740c88727
jenny20050401 <jenny20050401@gmail.com>
2025-04-24 11:28:56 +0000

    附近餐廳

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from linebot.models import (
     ImageSendMessage)
 from linebot.exceptions import InvalidSignatureError
 import logging
+from places import get_nearby_restaurants
 
 # 加載 .env 文件中的變數
 load_dotenv()
@@ -61,7 +62,12 @@ def handle_message(event: Event):
         app.logger.info(f"收到的訊息: {user_message}")
 
         # 使用 GPT 生成回應
-        reply_text = ("你說了：" + user_message)
+        if user_message == "附近的餐廳":
+            reply_text = get_nearby_restaurants()
+        elif user_message == "課表":
+            reply_text = "這是你的課表～"
+        else:
+            reply_text = ("你說了：" + user_message)
 
         line_bot_api.reply_message(
             event.reply_token,
